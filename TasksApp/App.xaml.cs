dd4855cb9864aeaf8bbd5143f87326ee8bfb67a8
Wandile Gwebu <wandilegwebu8@gmail.com>
2024-02-03 16:51:32 +0200

    Add project files.

---
+++ TasksApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TasksApp
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ TasksApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TasksApp
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		
+	}
+
+}
