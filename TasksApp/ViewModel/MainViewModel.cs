d38a51f94259cc3848fceaff77477388f8b9d04d
Wandile Gwebu <wandilegwebu8@gmail.com>
2024-02-12 19:41:25 +0200

    added the ViewModel class

---
+++ TasksApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace TasksApp.ViewModel
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+
+		// need to understand this
+		string text;
+
+		public string Text
+		{
+			get => text;
+			set
+			{
+				text = value;
+				OnPropertyChange(nameof(Text));
+			}
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		void OnPropertyChange(string name) => 
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+}
